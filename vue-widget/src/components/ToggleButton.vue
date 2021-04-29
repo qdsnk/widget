a8b51ad8496c3517f2395265e82b0ee5af15de21
qdsnk <iztem@inbox.ru>
2021-04-29 23:32:30 +0300

    'old-removed'

--- vue-widget/src/components/ToggleButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <div class="rate_quality"
-         onclick="document.getElementById('regForm').classList.toggle('open')">
-      <p>ОЦЕНИТЕ КАЧЕСТВО</p>
-    </div>
-  </div>
-</template>
